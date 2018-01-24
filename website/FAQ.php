4566f2f880dc9ba7fb0db67786745c1827e83d42
Peter bock <bockpeter@hotmail.com>
2018-01-24 22:44:57 +0100

    Update FAQ.php
    added the FAQ contents

--- website/FAQ.php
+++ website/FAQ.php
@@ -22,7 +22,119 @@
 
 		<div class="container" style="width: 75%;">
 			<h3 class="my-4">FAQ</h3>
+			<h4 class="my-4">What kind of posts are approved?</h4>
+			
+			<p>
+				SteemMakers focuses on both the <a href="https://en.wikipedia.org/wiki/Maker_culture" class="text-white" style="text-decoration: underline;">maker</a> and <a href="https://en.wikipedia.org/wiki/Do_it_yourself" class="text-white" style="text-decoration: underline;">do-it-yourself</a> movement.
+				<br>
+				With DIY we mean all activity to create or modify anything when it is accomplished by an individual rather than by a paid professional.
+				<br>
+				The makers movement is a technology driven subgroup of the DIY movement. It focuses on the creation of new devices as well as tinkering with existing ones. 
+			</p>
+            
+			<h4 class="my-4">What does SteemMakers do?</h4>
+			
+			<p>
+				We aim to be a guiding and helping community, supporting makers and DIYers on the steem blockchain in various ways. Have a look at our <a href="about.php" class="text-white" style="text-decoration: underline;">about page</a> for more details.
+			</p>
+            
+			<h4 class="my-4">My posts contain pictures of awesome craftwork I’ve seen on the Internet, will I be featured by SteemMakers?</h4>
+			
+			<p>
+				SteemMakers supports the makers themselves, so you need to be the one who made it, or demonstrate in your post that what you made is a derivative work with your own original touch to it.
+			</p>
 
+			<h4 class="my-4">My post has tons of images of my project, and nothing else, is it acceptable for SteemMakers ?</h4>
+			
+			<p>
+				It’s very good that you’ve shown your project under every angle, but we try to find projects that truly educate the readers as to how it was made, what materials were required, and how it was all put together.
+			</p>
+			
+			<h4 class="my-4">My post has only a video of my project, and nothing else, is it acceptable for SteemMakers ?</h4>
+			
+			<p>
+				We prefer video posts that at least have a short description. 
+				It also depends on the content of the video. 
+				Basically the same approach as for pictures is applicable for videos.
+			</p>
+			
+			<h4 class="my-4">My post is about a project I collaborated on, not my individual project, is it also acceptable?</h4>
+			
+			<p>
+				Of course it is.
+			</p>
+			
+			<h4 class="my-4">I’ve got a backlog of 80 projects on the Steemit platform, will you vote for them ?</h4>
+			
+			<p>
+				Given how the Steem blockchain works, the SteemMakers account will only vote for posts still in their active reward phase. 
+				This means SteemMakers will only vote and feature posts under 6 days old.
+			</p>
+			
+			<h4 class="my-4">I made a superb project, but it’s written in a language different to English, is it still eligible for SteemMakers ?</h4>
+			
+			<p>
+				At this moment we are mainly focusing on English posts.
+			</p>
+			
+			<h4 class="my-4">I’ve published my post on other internet sites, do I still apply?</h4>
+			
+			<p>
+				We focus on content that is original and has not been published anywhere before. 
+				SteemMakers obviously focuses on the Steem blockchain first.
+			</p>
+			
+			<h4 class="my-4">I followed a tutorial and made a post about it, is that acceptable?</h4>
+				
+			<p>
+				Your post can’t be a simple show-and-tell of how you finished a tutorial project that already existed. 
+				Exceptions can be made if you add to the original tutorial that for example has further details or different steps that leads to a different result.
+			</p>
+			
+			<h4 class="my-4">How can I submit posts?</h4>
+			
+			<p>
+				SteemMakers works as a curation guild. This means we search for valuable posts ourselves. 
+				You can request a review by posting a link in the request-review channel on our <a href="https://discord.gg/YJcAu8m" class="text-white" style="text-decoration: underline;">Discord server</a>.
+			
+			</p>
+			
+			<h4 class="my-4">Can I submit my own posts on the request-review channel?</h4>
+			
+			<p>
+				Yes, you can.
+			</p>
+			
+			<h4 class="my-4">How can I increase my chances of discovery?</h4>
+			
+			<p>
+				You can increase your chances of discovery by using the ‘steemmakers’ tag when posting.
+			</p>
+			
+			<h4 class="my-4">What happens after I submit a post?</h4>
+			
+			<p>
+				There is no guarantee your post will get featured. 
+				It is entirely up to the team to decide on which posts are considered.
+			</p>
+			
+			<h4 class="my-4">How many posts can I submit?</h4>
+			
+			<p>	
+				<i>As many as you can make.</i>
+			</p>
+			
+			<h4 class="my-4">Is there any other way I can get SteemMakers to vote for my brilliant work?</h4>
+			
+			<p>	
+				No.
+			</p>
+			
+			<h4 class="my-4">I have another question, where can I ask it?</h4>
+			
+			<p>	
+				The best way to reach us is to join our <a href="https://discord.gg/YJcAu8m" class="text-white" style="text-decoration: underline;">Discord server</a>.
+			</p>
 		</div>
 		<?php include("footer.php"); ?>
 	</body>
