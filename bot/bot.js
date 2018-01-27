75aebbe9dcc212648e94caafe08449e2dbdef1c0
Unknown <bruno.vermeersch@outlook.com>
2018-01-27 08:08:44 +0100

    Fixed comment permlinks in bot

--- bot/bot.js
+++ bot/bot.js
@@ -12,47 +12,56 @@ steem.api.setOptions({ url: 'https://api.steemit.com' });
 var config;
 var mysqlConnection;
 
-function createComment(id, author, permlink)
+function createCommentPermlink(parentAuthor, parentPermlink)
 {
+	var permlink;
+	// comments: re-parentauthor-parentpermlink-time
+	const timeStr = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '');
+	parentPermlink = parentPermlink.replace(/(-\d{8}t\d{9}z)/g, '');
+	permlink = `re-${parentAuthor}-${parentPermlink}-${timeStr}`;
 
-}
+	if (permlink.length > 255)
+	{
+	// STEEMIT_MAX_PERMLINK_LENGTH
+	permlink = permlink.substring(permlink.length - 255, permlink.length);
+	}
 
-function createVote(id, author, permlink)
-{
-
+	// only letters numbers and dashes shall survive
+	permlink = permlink.toLowerCase().replace(/[^a-z0-9-]+/g, '');
+	return permlink;
 }
 
 function main()
 {
-    config = JSON.parse(fs.readFileSync('./config/config.json', 'utf8'));
+	config = JSON.parse(fs.readFileSync('./config/config.json', 'utf8'));
 
-    mysqlConnection = mysql.createConnection({
-        host: config.host,
-        user: config.user,
-        password: config.password,
-        database: config.database
-    });
+	mysqlConnection = mysql.createConnection({
+		host: config.host,
+		user: config.user,
+		password: config.password,
+		database: config.database
+	});
 
-    mysqlConnection.connect(function(error)
-    {
-        if (error)
-        {
-            console.log("Failed to connect to database");
-            throw error;
-        }
-        else
-        {
-            console.log("Connected to database!");
+	mysqlConnection.connect(function(error)
+	{
+		if (error)
+		{
+			console.log("Failed to connect to database");
+			throw error;
+		}
+		else
+		{
+			console.log("Connected to database!");
 
-            mysqlConnection.query("SELECT approved_posts.id, name AS author_name, permlink FROM approved_posts JOIN users ON approved_posts.author_id = users.id WHERE (approved_posts.commented_on IS NULL AND approved_posts.commented_on IS NULL)", function (error, rows, fields)
-            {
-                if (error)
-                {
-                    console.log("Failed to query approved posts");
-                    throw error;
-                }
-                else
-                {
+			mysqlConnection.query("SELECT approved_posts.id, name AS author_name, permlink FROM approved_posts JOIN users ON approved_posts.author_id = users.id WHERE (approved_posts.commented_on IS NULL AND approved_posts.commented_on IS NULL)", function (error, rows, fields)
+			{
+			if (error)
+				{
+					console.log("Failed to query approved posts");
+					throw error;
+				}
+				else
+				{
 					var currentEntry = 0;
 					var totalEntries = rows.length;
 
@@ -69,8 +78,6 @@ function main()
 						var currentAuthor = rows[currentEntry].author_name;
 						var currentPermlink = rows[currentEntry].permlink;
 						console.log("Entry " + currentEntry + ": id=" + currentId + ", author name=" + currentAuthor + ", permlink=" + currentPermlink);
-
-						var commentPermlink = steem.formatter.commentPermlink(currentAuthor, currentPermlink);
  
 						var wif = steem.auth.toWif(config.votingAccount, config.votingAccountPW, 'posting');
 
@@ -95,7 +102,7 @@ function main()
 									}
 								});
 
-								var commentPermlink = steem.formatter.commentPermlink(currentAuthor, currentPermlink);
+								var commentPermlink = createCommentPermlink(currentAuthor, currentPermlink);
 								body = "Congratulations! This post has been upvoted by SteemMakers. We are a community based project that aims to support makers and DIYers on the blockchain in every way possible. Find out more about us on our website: [www.steemmakers.com](www.steemmakers.com). <br/><br/>If you like our work, please consider upvoting this comment to support the growth of our community. Thank you.";
 
 								steem.broadcast.comment(wif,  currentAuthor, currentPermlink, config.votingAccount, commentPermlink, "", body, "", function(err, result)
