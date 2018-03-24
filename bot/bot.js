2bb84c1548ec975001308b6d03b6dce720cd24cf
Doki <sdoktor@web.de>
2018-03-24 13:32:15 +0100

    Updated Bot comment text and added img/comment_logo_diy.png and img/comment_logo_maker.png Text config per category now in bot/comment.json

--- bot/bot.js
+++ bot/bot.js
@@ -34,6 +34,7 @@ function createCommentPermlink(parentAuthor, parentPermlink)
 function main()
 {
 	config = JSON.parse(fs.readFileSync('./config/config.json', 'utf8'));
+	comment = JSON.parse(fs.readFileSync('./comments.json', 'utf8'));
 
 	mysqlConnection = mysql.createConnection({
 		host: config.host,
@@ -53,7 +54,7 @@ function main()
 		{
 			console.log("Connected to database!");
 
-			mysqlConnection.query("SELECT approved_posts.id, name AS author_name, permlink FROM approved_posts JOIN users ON approved_posts.author_id = users.id WHERE (approved_posts.commented_on IS NULL AND approved_posts.commented_on IS NULL)", function (error, rows, fields)
+			mysqlConnection.query("SELECT approved_posts.id, name AS author_name, permlink, category_id FROM approved_posts JOIN users ON approved_posts.author_id = users.id WHERE (approved_posts.commented_on IS NULL AND approved_posts.commented_on IS NULL)", function (error, rows, fields)
 			{
 			if (error)
 				{
@@ -77,6 +78,7 @@ function main()
 						var currentId = rows[currentEntry].id;
 						var currentAuthor = rows[currentEntry].author_name;
 						var currentPermlink = rows[currentEntry].permlink;
+						var categoryId = rows[currentEntry].category_id;
 						console.log("Entry " + currentEntry + ": id=" + currentId + ", author name=" + currentAuthor + ", permlink=" + currentPermlink);
  
 						var wif = steem.auth.toWif(config.votingAccount, config.votingAccountPW, 'posting');
@@ -103,8 +105,16 @@ function main()
 								});
 
 								var commentPermlink = createCommentPermlink(currentAuthor, currentPermlink);
-								body = "Congratulations! This post has been upvoted by SteemMakers. We are a community based project that aims to support makers and DIYers on the blockchain in every way possible. Find out more about us on our website: [www.steemmakers.com](www.steemmakers.com). <br/><br/>If you like our work, please consider upvoting this comment to support the growth of our community. Thank you.";
-
+								// get category based comment from comment.json 
+								if (typeof comment[categoryId] !== 'undefined'  || omment[categoryId] !== null)
+								{
+									body = comment[categoryId];
+								}
+								else	
+								{
+									body = comment["default"];
+								}
+								
 								steem.broadcast.comment(wif,  currentAuthor, currentPermlink, config.votingAccount, commentPermlink, "", body, "", function(err, result)
 								{
 									// Comment callback
